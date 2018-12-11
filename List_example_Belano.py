38984e48e819d3e5fc785e259143481c19c7815c
jigbelano <45375428+jigbelano@users.noreply.github.com>
2018-12-11 11:52:03 +0800

    Create List_example_Belano.py

---
+++ List_example_Belano.py
@@ -0,0 +1,33 @@
+print("Iteration of List: ")
+foodList = ['Fries', 'Isaw' , 'Burger' , 'Nachos']
+for x in foodList:
+    print(x)
+print("...")
+drinkList = ['Coca Cola' , 'Sprite' , 'Royal', 'Rc']
+for x in drinkList:
+    print(x)
+print("...")
+nameList = ['Benoyo' , 'Bejerano' , 'Belano' , 'Bravo']
+for x in nameList:
+    print(x)
+print("...")
+fruitList = ['Lanzones' , 'Grapes' , 'Mango' , 'Melon']
+for x in fruitList:
+    print(x)
+
+print("Iteration of List: ")
+foodList = ['Fries', 'Isaw' , 'Burger' , 'Nachos']
+for x in foodList:
+    print(x)
+print("...")
+drinkList = ['Coca Cola' , 'Sprite' , 'Royal', 'Rc']
+for x in drinkList:
+    print(x)
+print("...")
+nameList = ['Benoyo' , 'Bejerano' , 'Belano' , 'Bravo']
+for x in nameList:
+    print(x)
+print("...")
+fruitList = ['Lanzones' , 'Grapes' , 'mango' , 'Melon']
+for x in fruitList:
+    print(x)

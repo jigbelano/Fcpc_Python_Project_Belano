60653369121700f874b46fdccce3a29213180194
jigbelano <45375428+jigbelano@users.noreply.github.com>
2019-01-22 11:25:12 +0800

    Rename ACTIVITY_B_BELANO to ACTIVITY_B_BELANO.py

---
+++ ACTIVITY_B_BELANO.py
@@ -0,0 +1,23 @@
+def LIST(x):
+    listX = x
+
+    print(listX[0:7])
+listX = [1, 2, 3, 4, 5, 6, 7]
+LIST(listX)
+
+def LIST (y):
+    listY = y
+
+    print(listY[0:14])
+
+listY = [2,4,6,10,12,14,16,18]
+LIST(listY)
+
+def LIST(a):
+    listA = a
+
+    print(listA[0:21])
+
+listA = [3,6,9,12,15,18,21,]
+LIST(listA)
+
